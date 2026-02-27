315850f52c3a61d52264b588606fd1d9075c4e24
Holden Karau <holden@pigscanfly.ca>
2026-02-26 21:31:06 -0800

    Huzzah

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,32 @@
-$(document).ready(function() {
-  // Mobile nav toggle
-  $('.nav-toggle').on('click', function() {
-    $('.nav-menu').toggleClass('active');
-  });
+// Mobile nav toggle
+(function() {
+  var toggle = document.querySelector('.nav-toggle');
+  var menu = document.querySelector('.nav-menu');
 
-  // Close menu when clicking a link
-  $('.nav-menu a').on('click', function() {
-    $('.nav-menu').removeClass('active');
+  if (toggle && menu) {
+    toggle.addEventListener('click', function() {
+      var expanded = menu.classList.toggle('active');
+      toggle.setAttribute('aria-expanded', expanded);
+    });
+
+    // Close menu when clicking a link
+    var links = menu.querySelectorAll('a');
+    for (var i = 0; i < links.length; i++) {
+      links[i].addEventListener('click', function() {
+        menu.classList.remove('active');
+        toggle.setAttribute('aria-expanded', 'false');
+      });
+    }
+  }
+})();
+
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(function(anchor) {
+  anchor.addEventListener('click', function(e) {
+    var target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   });
 });
